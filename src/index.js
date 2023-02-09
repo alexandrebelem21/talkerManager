36520a6f9b402959c21aa3ea196e832c57a725e2
alexandrebelem21 <alexandrebelem21@gmail.com>
2023-02-09 17:07:48 -0300

    Feat: Create GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -21,3 +21,14 @@ app.get('/talker', async (req, res) => {
 
   return res.status(200).json(talkers);
 });
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  const talker = talkers.find((tlk) => tlk.id === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talker);
+});
